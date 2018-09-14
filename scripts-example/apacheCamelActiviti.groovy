63d24d428f79b518a9055c5a62928c1cf3c01f2f
452 <sp.titan@gmail.com>
2018-09-14 09:02:31 +0300

    update lib versions, update activiti example

--- scripts-example/apacheCamelActiviti.groovy
+++ scripts-example/apacheCamelActiviti.groovy
@@ -1,8 +1,8 @@
-@Grab('org.apache.camel:camel-core:2.20.1')
-@Grab('org.apache.camel:camel-slack:2.20.1')
-@Grab('org.apache.camel:camel-groovy:2.20.1')
+@Grab('org.apache.camel:camel-core:2.22.0')
+@Grab('org.apache.camel:camel-groovy:2.22.0')
 @Grab('org.activiti:activiti-camel:6.0.0')
-@Grab('org.slf4j:slf4j-simple:1.7.18')
+@Grab('com.h2database:h2:1.4.197')
+@Grab('org.slf4j:slf4j-simple:1.7.25')
 import org.apache.camel.*
 import org.apache.camel.impl.*
 import org.apache.camel.builder.*
@@ -25,32 +25,40 @@ import org.activiti.engine.*
 def camelContext = new DefaultCamelContext()
 String fromActivitiEndPoint = "activiti:testCamelTask:sendMsgToCamel?copyCamelBodyToBody=true";
 
-// ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault()
-                .buildProcessEngine();
+ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+//ProcessEngine processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResourceDefault().buildProcessEngine();
 // Get main service interfaces
 RepositoryService repositoryService = processEngine.getRepositoryService();
 RuntimeService runtimeService = processEngine.getRuntimeService();
 HistoryService historyService = processEngine.getHistoryService();
 // Deploy intro process definition
-repositoryService.createDeployment().name("intro").addClasspathResource("intro.bpmn20.xml").deploy();
+repositoryService.createDeployment().name("OrderProcess").addClasspathResource("OrderProcess.bpmn20.xml").deploy();
 
 org.activiti.camel.SimpleContextProvider activiti = new org.activiti.camel.SimpleContextProvider('OrderProcess', camelContext)
-// activiti.setValue('OrderProcess')
-// activiti.setRef('camelContext')
+//activiti.setValue('OrderProcess')
+//activiti.setRef('camelContext')
 def jndiContext = new JndiContext();
 jndiContext.bind("activiti", activiti)
 
 camelContext.addRoutes(new RouteBuilder() {
     def void configure() {
         Helper helper = new Helper();
-        from("timer://logOutputTimer?period=3000")
-            // .transform()
-            // .simple('new ${date:now:yyyyMMdd} ${in.header.number} range 100..199')
-            // .process({ println "Hello World!" + it.in.body.reverse()} as org.apache.camel.Processor)
+        from("timer://logOutputTimer?period=10000")
             .setBody(bean(helper))
-            .setProperty(PROCESS_KEY_PROPERTY, simple('OrderProcess.bpmn20.xml'))
-            .to("activiti:OrderProcess")
+            .setProperty(PROCESS_KEY_PROPERTY, simple('OrderProcess'))
+            //.setProperty("PROCESS_ID_PROPERTY", simple('OrderProcess'))
+            .to('log:aaa?showAll=true')
+            .log('Process to handle incoming order file has been started (process instance id ${body})')
+            //.to('activiti:OrderProcess:receiveDelivery');
+            .to("activiti:OrderProcess");
+            
+        from("activiti:OrderProcess:processOrder")
+            .log('Processing order ${property.orderid} created on ${property:timestamp}')
+            .log('  original message: ${property.message}');
+
+        from("activiti:OrderProcess:processDelivery")
+            .log('Processing delivery for order ${property.orderid} created on ${property:timestamp}')
+            .log('  original message: ${property.message}');
     }
 })
 camelContext.start()
@@ -59,16 +67,15 @@ addShutdownHook{ camelContext.stop() }
 synchronized(this){ this.wait() }
 
 public class Helper {
-  /*
-   * This method will extract information from the Exchange (using Camel annotations) and put them in a
-   * Map that will be used for setting up the process' variables.
-   */
-  @Handler
-  public Map getProcessVariables(@Simple('${date:now:yyyy-MM-dd kk:mm:ss}') String timestamp) {
-      Map<String, Object> variables = new HashMap<String, Object>();
-      variables.put('message', 'testBody');
-      variables.put('orderid', 'testIds');
-      variables.put('timestamp', timestamp);
-      return variables;
-  }
+
+        @Handler
+        public Map getProcessVariables(@Body String body,
+                                       @Header(Exchange.FILE_NAME) String filename,
+                                       @Simple('${date:now:yyyy-MM-dd kk:mm:ss}') String timestamp) {
+            Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("message", body);
+            variables.put("orderid", filename);
+            variables.put("timestamp", timestamp);
+            return variables;
+        }
 }
